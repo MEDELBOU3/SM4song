78744fb52a30a3d4a2e5722c9b15aac10a5aeffd
Mohamed El bouanani <154015011+MEDELBOU3@users.noreply.github.com>
2024-12-03 16:56:40 +0100

    Update weather.js

--- src/weather/weather.js
+++ src/weather/weather.js
@@ -76,7 +76,7 @@
         }
         .weather-content{
             overflow-y: auto;
-            height: 80vh;
+            height: 83vh;
             padding: 10px;
             background-color: var(--spotify-black);
         }
